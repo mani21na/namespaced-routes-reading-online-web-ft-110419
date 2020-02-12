b28e1a89c64d8f978e321f119fa516574ac06a18
Hanna Lee <mani21na@gmail.com>
2020-02-12 14:18:43 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
   get '/admin/stats', to: 'stats#index'
 
   root 'posts#index'
+
+  get '/stats', to: 'stats#index'
+
+  get '/admin/stats', to: 'stats#index'
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 end
